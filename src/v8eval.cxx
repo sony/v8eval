f801a14b4f05d67787add04792586762b3bbd5b7
Gianluca Guida <glguida@tlbflush.org>
2015-10-26 14:03:57 +0000

    v8eval: add debug server

--- src/v8eval.cxx
+++ src/v8eval.cxx
@@ -1,4 +1,5 @@
 #include "v8eval.h"
+#include "dbgsrv.h"
 
 #include <stdlib.h>
 #include <string.h>
@@ -61,6 +62,16 @@ _V8::_V8() {
   create_params.array_buffer_allocator = &allocator;
   isolate_ = v8::Isolate::New(create_params);
 
+  dbg_server_ = nullptr;
+  dbg_isolate_ = nullptr;
+  callback_ = nullptr;
+  callbackopq_ = nullptr;
+
+  // Use Isolate's local storage to store a pointer to the associated
+  // V8 instance. This is retrieved in the V8 debugger's callback, as
+  // the instance pointer is the only context we get from the caller.
+  isolate_->SetData(0, this);
+
   v8::Locker locker(isolate_);
 
   v8::Isolate::Scope isolate_scope(isolate_);
@@ -69,8 +80,11 @@ _V8::_V8() {
 }
 
 _V8::~_V8() {
-  context_.Reset();
+  if (dbg_server_ != nullptr) {
+    delete dbg_server_;
+  }
 
+  context_.Reset();
   isolate_->Dispose();
 }
 
@@ -180,6 +194,92 @@ std::string _V8::call(const std::string& func, const std::string& args) {
   } else {
     return to_std_string(json_stringify(context, result));
   }
+}
+
+bool _V8::debugger_enable(int port) {
+  if (dbg_server_ != nullptr) {
+    return false;
+  }
+  dbg_server_ = new DbgSrv(*this);
+  if (!dbg_server_->start(port)) {
+    delete dbg_server_;
+    dbg_server_ = nullptr;
+    return false;
+  }
+  return true;
+}
+
+void _V8::debugger_disable() {
+  if (dbg_server_ != nullptr) {
+    delete dbg_server_;
+    dbg_server_ = nullptr;
+  }
+}
+
+void _V8::debugger_message_handler(const v8::Debug::Message& message) {
+  v8::Isolate* isolate = message.GetIsolate();
+  _V8 *_v8 = (v8eval::_V8*)isolate->GetData(0);
+  std::string string = *v8::String::Utf8Value(message.GetJSON());
+
+  if (_v8->callback_ != nullptr) {
+    _v8->callback_(string, _v8->callbackopq_);
+  }
+}
+
+bool _V8::debugger_init(debugger_cb cb, void *cbopq) {
+  v8::Isolate::CreateParams create_params;
+  create_params.array_buffer_allocator = &allocator;
+  dbg_isolate_ = v8::Isolate::New(create_params);
+
+  if (callback_ != nullptr) {
+    return false;
+  }
+  callback_ = cb;
+  callbackopq_ = cbopq;
+
+  // Set Debuger callback.
+  v8::Locker locker(isolate_);
+  v8::Isolate::Scope isolate_scope(isolate_);
+  v8::HandleScope handle_scope(isolate_);
+  v8::Debug::SetMessageHandler(debugger_message_handler);
+
+  return true;
+}
+
+void _V8::debugger_process() {
+  // Process debug messages on behalf of the V8 instance.
+  v8::Locker locker(isolate_);
+  v8::Isolate::Scope isolate_scope(isolate_);
+  v8::HandleScope handle_scope(isolate_);
+
+  v8::Debug::ProcessDebugMessages();
+}
+
+bool _V8::debugger_send(const std::string& cmd) {
+  v8::Locker locker(dbg_isolate_);
+  v8::Isolate::Scope isolate_scope(dbg_isolate_);
+  v8::HandleScope handle_scope(dbg_isolate_);
+
+  if (callback_ == nullptr) {
+    return false;
+  }
+
+  v8::Local<v8::String> vstr = v8::String::NewFromUtf8(dbg_isolate_, cmd.c_str(), v8::NewStringType::kNormal).ToLocalChecked();
+  v8::String::Value v(vstr);
+  v8::Debug::SendCommand(isolate_, *v, v.length());
+  return true;
+}
+
+void _V8::debugger_stop() {
+  v8::Locker locker(isolate_);
+  v8::Isolate::Scope isolate_scope(isolate_);
+  v8::HandleScope handle_scope(isolate_);
+  v8::Debug::SetMessageHandler(nullptr);
+
+  callback_ = nullptr;
+  callbackopq_ = nullptr;
+  dbg_isolate_->Dispose();
+  dbg_isolate_ = nullptr;
 }
 
 }  // namespace v8eval
--- src/v8eval.h
+++ src/v8eval.h
@@ -4,9 +4,12 @@
 #include <string>
 
 #include "v8.h"
+#include "v8-debug.h"
 
 /// \file
 namespace v8eval {
+
+class DbgSrv;
 
 /// \brief Initialize the V8 runtime environment
 /// \return success or not as boolean
@@ -19,6 +22,8 @@ bool initialize();
 ///
 /// This method disposes the V8 runtime environment.
 bool dispose();
+
+ typedef void (*debugger_cb)(std::string&, void *opq);
 
 /// \class _V8
 ///
@@ -48,15 +53,44 @@ class _V8 {
   /// If some JavaScript exception happens in runtime, the exception message is returned.
   std::string call(const std::string& func, const std::string& args);
 
+  /// \brief Start a debug server associated with the V8 instance
+  /// \param port The TCP/IP port the debugger will listen, at localhost
+  /// \return success or not as boolean
+  ///
+  /// After the debugger is successfully started, it will be possible to
+  /// send commands and receive events at the specified port. When the
+  /// debugger is started, the Javascript's "debugger" statement will
+  /// cause the V8 instance to halt and wait for instructions through
+  /// the debugger port.
+  bool debugger_enable(int port);
+
+  /// \brief Stop the debug server, if running.
+  ///
+  /// The debug server, if currently running, will be stopped, causing
+  /// connections to remote debuggers to be dropped.
+  void debugger_disable();
+
  private:
+  static void debugger_message_handler(const v8::Debug::Message& message);
   v8::Local<v8::Context> new_context();
   v8::Local<v8::String> new_string(const char* str);
   v8::Local<v8::Value> json_parse(v8::Local<v8::Context> context, v8::Local<v8::String> str);
   v8::Local<v8::String> json_stringify(v8::Local<v8::Context> context, v8::Local<v8::Value> value);
 
+  bool debugger_init(debugger_cb cb, void *cbopq);
+  bool debugger_send(const std::string& cmd);
+  void debugger_process();
+  void debugger_stop();
+
  private:
   v8::Isolate* isolate_;
   v8::Persistent<v8::Context> context_;
+  class DbgSrv *dbg_server_;
+  v8::Isolate* dbg_isolate_;
+  debugger_cb callback_;
+  void *callbackopq_;
+
+  friend class DbgSrv;
 };
 
 }  // namespace v8eval
