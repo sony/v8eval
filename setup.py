ddba3a314610c8da7c26561d8f262a6f6a65a01a
Prateek Papriwal <prateek.papriwal@jp.sony.com>
2015-12-18 14:52:43 +0900

    set clang as compiler during pip install

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
-from os import system
+from os import system, environ
 from os.path import abspath, dirname, exists
 from sys import platform
 
@@ -26,27 +26,32 @@ system("cat " + py_dir + "/_v8eval.py >> " + py_v8eval_dir + "/v8eval.py")
 
 # build _v8eval.so
 include_dirs = [v8_dir, v8_dir + '/include', uv_dir + '/include']
+library_dirs = [v8eval_root + '/build', uv_dir + '/.libs']
+libraries=['v8eval',
+           'v8_libplatform',
+           'v8_base',
+           'v8_libbase',
+           'v8_nosnapshot',
+           'icui18n',
+           'icuuc',
+           'icudata',
+           'uv']
 
-library_dirs = [v8eval_root + '/build']
 if platform == "linux" or platform == "linux2":
+    environ["CC"] = v8_dir + '/third_party/llvm-build/Release+Asserts/bin/clang'
+    environ["CXX"] = v8_dir + '/third_party/llvm-build/Release+Asserts/bin/clang++'
+
+    libraries += ['rt']
+
     library_dirs += [v8_dir + '/out/x64.release/obj.target/tools/gyp',
                      v8_dir + '/out/x64.release/obj.target/third_party/icu']
 elif platform == "darwin":
     library_dirs += [v8_dir + '/out/x64.release']
-library_dirs += [uv_dir + '/.libs']
 
 v8eval_module = Extension(
     '_v8eval',
     sources=[v8eval_root + '/python/v8eval/v8eval_wrap.cxx'],
-    libraries=['v8eval',
-               'v8_libplatform',
-               'v8_base',
-               'v8_libbase',
-               'v8_nosnapshot',
-               'icui18n',
-               'icuuc',
-               'icudata',
-               'uv'],
+    libraries=libraries,
     include_dirs=include_dirs,
     library_dirs=library_dirs,
     extra_compile_args=['-O3',
