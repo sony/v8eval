8d6aa49ca9418f1a18fa07f7aa574cea0442d293
Yoshiyuki Mineo <Yoshiyuki.Mineo@jp.sony.com>
2017-10-11 12:02:46 +0900

    Bump version to 0.2.10

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ except ImportError:
 
 # setup v8eval package
 setup(name='v8eval',
-      version='0.2.9',
+      version='0.2.10',
       author='Yoshiyuki Mineo',
       author_email='Yoshiyuki.Mineo@jp.sony.com',
       license='MIT',
